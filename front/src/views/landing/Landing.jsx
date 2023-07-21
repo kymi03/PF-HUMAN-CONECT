7b9559dfe039096c7aa19b541f63403c1814ddc2
pickless141 <masiivan23@gmail.com>
2023-07-21 11:53:45 -0400

    Creacion de footer y link a home

--- front/src/views/landing/Landing.jsx
+++ front/src/views/landing/Landing.jsx
@@ -1,18 +1,30 @@
 import { Link } from "react-router-dom";
+//import SliderLanding from "../../components/sliders/SliderLanding";
 
 export default function Landing() {
+  
   return (
-    <div className="bg-[url('../src/assets/Cover-Home.jpg')] bg-cover bg-center h-full w-full mt-auto mb-auto">
-      <div className="flex justify-center items-center mt-15% ml-50% flex-col">
-        <div className="">
-          <h1>Bienvenidos!</h1>
-        </div>
-        <div>
-          <Link to="/home">
-            <button className="">Click para entrar!</button>
-          </Link>
-        </div>
+    <div className=" bg-cover bg-center overflow-hidden bg-amber-200">
+      <div>
+        <p className="absolute top-0 left-0 text-black text-7xl font-bold">HUMAN CONET</p>
       </div>
+      
+      {/* <div className=""> aqui iria el slider 
+        <SliderLanding/>
+      </div> */}
+    
+      <div className="absolute bottom-32 right-4 text-black">
+        <p className="text-7xl font-bold">JUNTOS SOMOS</p>
+      </div>
+      <footer className="absolute bottom-0 left-0 w-full text-center py-5 bg-gray-800 text-white flex justify-between items-center">
+        <div>
+          <img src='/assets/icons/turtle-turquoise.png' alt="turtle icon" className="w-14 h-14" />
+        </div>
+        <Link to="/home" className="no-underline text-3xl">CLICK AQUÍ PARA COMENZAR YA</Link>
+        <div>
+          <img src='/assets/icons/turtle-turquoise.png' alt="turtle icon" className="w-14 h-14" />
+        </div>
+      </footer>
     </div>
   );
 }
