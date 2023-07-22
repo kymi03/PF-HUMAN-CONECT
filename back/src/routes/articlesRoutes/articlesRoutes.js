b659fb8c3f5e32dbd5cf522dfa36df2b7fd16f70
NazaRosales <nazarenorosales_@hotmail.com>
2023-07-22 16:05:30 -0300

    Finalizado el CRUD  de la ruta /articles

--- back/src/routes/articlesRoutes/articlesRoutes.js
+++ back/src/routes/articlesRoutes/articlesRoutes.js
@@ -2,13 +2,13 @@ const { Router } = require("express");
 const getArticlesController = require("../../controllers/articlesControllers/getArticlesController");
 const postArticlesController = require("../../controllers/articlesControllers/postArticlesController");
 const putArticlesController = require("../../controllers/articlesControllers/putArticlesController");
-// const deleteArticlesController = require("../../controller/articlesControllers/deleteArticlesController");
+const deleteArticleController = require("../../controllers/articlesControllers/deleteArticlesController");
 
 const articleRoutes = Router();
 
 articleRoutes.get("/", getArticlesController);
 articleRoutes.post("/", postArticlesController);
 articleRoutes.put("/", putArticlesController);
-// articleRoutes.delete("/", deleteArticlesController);
+articleRoutes.delete("/", deleteArticleController);
 
 module.exports = articleRoutes;
