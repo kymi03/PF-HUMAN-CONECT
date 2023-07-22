52d61f2c467eee00307649edbc56c029c859be51
Daniel Fuentes <danielfuentesri@gmail.com>
2023-07-22 15:06:10 -0500

    Crud de Projects hecho

--- back/src/controllers/projectsControllers/getProjectsController.js
+++ back/src/controllers/projectsControllers/getProjectsController.js
@@ -1,1 +1,37 @@
-console.log("hello world");
+const projectModel = require("./../../models/project");
+
+const getProjectController = async (req, res) => {
+  const { name, id, location, author } = req.query;
+  try {
+    // En caso de que se reciban parametros por query estos serán utilizados para
+    //filtrar la información de la base de datos
+    const query = {};
+    id && (query.id = id);
+    name && (query.name = { $regex: new RegExp(name, "i") });
+    location && (query.location = location);
+    author && (query.author = author);
+
+    //se realiza la peticion a la base de datos y si todo sale bien se
+    //responde con un arreglo de proyectos
+    if (query.id) {
+      const projects = await projectModel.findById(id);
+      projects
+        ? res.status(200).json(projects)
+        : res.status(404).json({
+            error: `No se encontró el proyecto con id: ${id}`,proyectos
+          });
+    } else {
+      const projects = await projectModel.find(query);
+      projects.length
+        ? res.status(200).json(projects)
+        : res.status(404).json({
+            error: "No se encontraron proyectos",
+          });
+    }
+  } catch (error) {
+    console.error("Error al requerir proyectos", error);
+    res.status(500).json({ error: "Error al requerir proyectos", error });
+  }
+};
+
+module.exports = getProjectController;
--- back/src/routes/routesIndex.js
+++ back/src/routes/routesIndex.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
 const mainRoute = Router();
 const articleRoutes = require("./articlesRoutes/articlesRoutes")
+const projectRoutes = require ("./projectsRoutes/projectsRoutes")
 
 
 mainRoute.use("/articles", articleRoutes)
+
+mainRoute.use("/projects", projectRoutes)
 
 module.exports = mainRoute;
