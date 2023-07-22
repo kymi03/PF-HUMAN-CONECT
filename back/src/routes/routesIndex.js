6eb5f4d4acbecebe12d42477faf8495acb6fe168
kymi03 <kimysabela03@gmail.com>
2023-07-22 17:18:01 -0500

    ruta put e indexes funcionales

--- back/src/routes/routesIndex.js
+++ back/src/routes/routesIndex.js
@@ -3,7 +3,8 @@ const mainRoute = Router();
 const articleRoutes = require("./articlesRoutes/articlesRoutes");
 const documentariesRoutes = require("./docsRoutes/docsRoutes");
 const adminsRoutes = require("./adminsRoutes/adminsRoutes");
-const userRoutes = require('./userRoutes/userRoutes')
+const userRoutes = require('./userRoutes/userRoutes');
+
 
 mainRoute.use("/articles", articleRoutes);
 mainRoute.use("/documentaries", documentariesRoutes);
--- back/src/routes/userRoutes/userRoutes.js
+++ back/src/routes/userRoutes/userRoutes.js
@@ -1,11 +1,15 @@
 const { Router } = require("express");
 const getUserController = require("../../controllers/usersControllers/getUserController");
-
+const postUserController = require('../../controllers/usersControllers/postUserController');
+const putUserController = require('../../controllers/usersControllers/putUserController');
+const deleteUserController = require('../../controllers/usersControllers/deleteUserController');
 
 const userRoutes = Router();
 
 
 userRoutes.get("/", getUserController);
-
+userRoutes.post("/", postUserController);
+userRoutes.put("/", putUserController);
+userRoutes.delete("/", deleteUserController);
 
 module.exports = userRoutes;
