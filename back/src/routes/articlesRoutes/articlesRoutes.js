bf1b0c12ca13eff3742f4aaf15e8661faead783b
NazaRosales <nazarenorosales_@hotmail.com>
2023-07-22 16:00:26 -0300

    Se creó la ruta PUT de articles
    La ruta put de articles busca por _id y actualiza solo la o las propiedades que llegan por body

--- back/src/routes/articlesRoutes/articlesRoutes.js
+++ back/src/routes/articlesRoutes/articlesRoutes.js
@@ -1,17 +1,14 @@
 const { Router } = require("express");
 const getArticlesController = require("../../controllers/articlesControllers/getArticlesController");
 const postArticlesController = require("../../controllers/articlesControllers/postArticlesController");
-// const putArticlesController = require("../../controller/articlesControllers/putArticlesController");
+const putArticlesController = require("../../controllers/articlesControllers/putArticlesController");
 // const deleteArticlesController = require("../../controller/articlesControllers/deleteArticlesController");
 
 const articleRoutes = Router();
-const postArticles = Router();
-// const putArticles = Router();
-// const deleteArticles = Router();
 
 articleRoutes.get("/", getArticlesController);
 articleRoutes.post("/", postArticlesController);
-// articleRoutes.put("/", putArticlesController);
+articleRoutes.put("/", putArticlesController);
 // articleRoutes.delete("/", deleteArticlesController);
 
 module.exports = articleRoutes;
