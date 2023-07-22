a4903589e1a3a21a8536c05bb6a20a8c4f58d2be
NazaRosales <nazarenorosales_@hotmail.com>
2023-07-22 15:28:53 -0300

    Ruta POST de articles funcionando

--- back/src/routes/articlesRoutes/articlesRoutes.js
+++ back/src/routes/articlesRoutes/articlesRoutes.js
@@ -1,16 +1,16 @@
 const { Router } = require("express");
 const getArticlesController = require("../../controllers/articlesControllers/getArticlesController");
-//const postArticlesController = require("../../controller/articlesControllers/postArticlesController");
+const postArticlesController = require("../../controllers/articlesControllers/postArticlesController");
 // const putArticlesController = require("../../controller/articlesControllers/putArticlesController");
 // const deleteArticlesController = require("../../controller/articlesControllers/deleteArticlesController");
 
 const articleRoutes = Router();
-//const postArticles = Router();
+const postArticles = Router();
 // const putArticles = Router();
 // const deleteArticles = Router();
 
 articleRoutes.get("/", getArticlesController);
-//articleRoutes.post("/", postArticlesController);
+articleRoutes.post("/", postArticlesController);
 // articleRoutes.put("/", putArticlesController);
 // articleRoutes.delete("/", deleteArticlesController);
 
