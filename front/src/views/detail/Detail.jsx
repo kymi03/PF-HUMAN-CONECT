2e47e9f271d9bbf14aaf5737944c05eca79d6b4c
171091 <txng@hotmail.com>
2023-07-24 02:35:42 -0600

    detail no finalizado

--- front/src/views/detail/Detail.jsx
+++ front/src/views/detail/Detail.jsx
@@ -1,43 +1,86 @@
+import styles from "./detail.module.css";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import NavBarAle from "../../components/NavBar/NavBar.ale";
 import FooterMoreInfo from "../../components/footer/FooterMoreInfo";
 
-
 function Detail() {
+  const [PAD, setPAD] = useState([]);
+  const { id } = useParams();
+  const { images = [], videos = [] } = PAD.media || {};
 
-
-const [PAD, setPAD] = useState([]);
-const { id } = useParams();
-
-useEffect(() => {
-  axios.get(`http://localhost:3001/projects?id=${id}`)
-    .then(({ data }) => {
+  useEffect(() => {
+    axios.get(`http://localhost:3001/projects?id=${id}`).then(({ data }) => {
       if (data.name) {
         setPAD(data);
       } else {
-        window.alert('No hay proyectos con ese ID');
+        window.alert("No hay proyectos con ese ID");
       }
     });
-  return setPAD({});
-}, []);
+    return setPAD({});
+  }, []);
 
-// console.log(PAD)
-return (
-   
-    
-
- 
-
-      <div >
-        <NavBarAle></NavBarAle>
-        <img src={PAD.media ? PAD.media.images[0].imageUrl : 'https://humanconet.org/wp-content/uploads/2022/09/Cover-Home-Human-Conet-01-1-1536x780.webp' }></img>
+  // console.log(PAD)
+  return (
+    <div>
+      <NavBarAle></NavBarAle>
+      <div className={styles["header-section"]}>
         <h1>{PAD.name}</h1>
-        <p>{PAD.body}</p>
-        <FooterMoreInfo></FooterMoreInfo>
+        <img
+          src={
+            images.length > 0
+              ? images[0].imageUrl
+              : "https://humanconet.org/wp-content/uploads/2022/09/Cover-Home-Human-Conet-01-1-1536x780.webp"
+          }
+        />
       </div>
-
+      <div className={styles["content-section"]}>
+        <p>{PAD.body}</p>
+        {/* Muestra todas las imágenes */}
+        {PAD.additionalImages &&
+          PAD.additionalImages.map((img, index) => (
+            <img
+              key={index}
+              src={img.imageUrl}
+              alt={`Additional Image ${index + 1}`}
+              className={styles["content-section"]}
+            />
+          ))}
+        {/* Muestra todos los videos */}
+        {videos.map((video, index) => (
+          <div key={index} className={styles["video-responsive"]}>
+            <div className={styles["video-container"]}>
+              {" "}
+              {/* Solo video-container aquí */}
+              {video.videoUrl.includes("youtube") ? (
+                <iframe
+                  className={styles["video-container-iframe"]} // Aplica la clase al iframe aquí
+                  src={`https://www.youtube.com/embed/${
+                    video.videoUrl.split("v=")[1]
+                  }`}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              ) : video.videoUrl.includes("vimeo") ? (
+                <iframe
+                  className={styles["video-container-iframe"]} // Aplica la clase al iframe aquí
+                  src={`https://player.vimeo.com/video/${
+                    video.videoUrl.split("/")[3]
+                  }`}
+                  frameBorder="0"
+                  allow="autoplay; fullscreen; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              ) : null}
+            </div>
+          </div>
+        ))}
+      </div>
+      <FooterMoreInfo></FooterMoreInfo>
+    </div>
   );
 }
 
