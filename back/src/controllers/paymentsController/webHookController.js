2b039be36007a08dafc7a0ff4df1bbfa64d1bdb6
NazaRosales <nazarenorosales_@hotmail.com>
2023-07-29 18:57:01 -0300

    Config de mercado pago

--- back/src/controllers/paymentsController/webHookController.js
+++
@@ -1,5 +0,0 @@
-const webHookController = (req, res) => {
-  res.send("webhook...");
-};
-
-module.exports = webHookController;
--- back/src/routes/payments/paymentRoutes.js
+++ back/src/routes/payments/paymentRoutes.js
@@ -1,20 +1,16 @@
 const { Router } = require("express");
 const paymentsController = require("../../controllers/paymentsController/paymentsController");
-const webHookController = require("../../controllers/paymentsController/webHookController");
+const successController = require("../../controllers/paymentsController/successController");
 
 const paymentRoutes = Router();
 
 paymentRoutes.post("/", paymentsController);
 
-paymentRoutes.get("/success", (req, res) => {
-  res.send("success");
-});
+paymentRoutes.get("/success", successController);
 paymentRoutes.get("/failure", (req, res) => {
   res.send("failure");
 });
 paymentRoutes.get("/pending", (req, res) => {
   res.send("pending");
 });
-paymentRoutes.get("/webhook", webHookController);
-
 module.exports = paymentRoutes;
