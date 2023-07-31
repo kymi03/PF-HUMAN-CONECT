50691b225ef1b0aa6a241800511c5d51c8bc1a96
NazaRosales <nazarenorosales_@hotmail.com>
2023-07-30 22:05:34 -0300

    Front y back de mercadopago para la 2da expo

--- front/src/views/donar/Donar.jsx
+++ front/src/views/donar/Donar.jsx
@@ -1,19 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBarAle from "../../components/NavBar/NavBar.ale";
-import Footer from '../../components/footer/Footer'
+import Footer from "../../components/footer/Footer";
+import axios from "axios";
+import styles from "./Donar.module.css";
+
 function Donar() {
+  const [unitPrice, setUnitPrice] = useState(500);
+  const [chart, setChart] = useState([]);
+
+  const sendChart = async (chart) => {
+    if (unitPrice < 500) return;
+    try {
+      const data = await axios
+        .post("http://localhost:3001/payments", { items: chart })
+        .then((res) => {
+          return res.data;
+        });
+      const URL = data.result.body.sandbox_init_point;
+      window.open(URL, "_blank");
+    } catch (error) {
+      alert(`No se pudo generar el link de pago. Error: ${error.message}`);
+    }
+  };
+
+  const handleChange = (e) => {
+    console.log(chart);
+    const amount = parseFloat(e.target.value);
+    const unitPrice = amount / chart.length;
+    setUnitPrice(unitPrice);
+
+    console.log(unitPrice);
+    setChart([
+      {
+        title: "Yurumanguí resiste",
+        unit_price: unitPrice,
+        currency_id: "ARS",
+        quantity: 1,
+      },
+      {
+        title: "Anchicayá resiste",
+        unit_price: unitPrice,
+        currency_id: "ARS",
+        quantity: 1,
+      },
+      {
+        title: "Yukpa resiste",
+        unit_price: unitPrice,
+        currency_id: "ARS",
+        quantity: 1,
+      },
+    ]);
+  };
+
   return (
-  <>
+    <>
+      <NavBarAle />
 
-    <NavBarAle />
-    
-    <div>
-     
-<h2>Aqui puedes donar , y saber cuanto acada causa</h2>
+      <div>
+        <h2>Aqui puedes donar , y saber cuanto acada causa</h2>
+      </div>
 
-    </div>
-    <Footer />
-  </>
+      <div className={styles["form-donativo"]}>
+        <form>
+          <h1>Realiza tu donativo.</h1>
+
+          <label> Voy a donar: $</label>
+          <input
+            type="number"
+            min={1500}
+            placeholder="1500"
+            onChange={handleChange}
+          />
+        </form>
+        <button onClick={() => sendChart(chart)}> DONAR ♥ </button>
+      </div>
+      <Footer />
+    </>
   );
 }
 
