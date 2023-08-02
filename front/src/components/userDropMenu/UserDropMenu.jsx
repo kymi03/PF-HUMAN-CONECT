d98a3965d71dccaf9143254d64eb6f3f5dfe5404
AlejoC137 <dapatinoa@unal.edu.co>
2023-08-01 21:42:14 -0500

    Refactor User Dropmenu 2

--- front/src/components/userDropMenu/UserDropMenu.jsx
+++ front/src/components/userDropMenu/UserDropMenu.jsx
@@ -55,7 +55,7 @@ const UserDropManu = () => {
 
           className=" h-10 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" >
           {  data ? data.name : 'user name' }
-        </button>
+        </button> 
 
         <img
           src="https://humanconet.org/wp-content/uploads/2022/09/Anchincaya-Resiste-HC-01-1024x1024.webp"
