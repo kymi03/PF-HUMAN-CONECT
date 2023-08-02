dfcd5190c7901beefa5fe8ff92cd083c31d46710
NazaRosales <nazarenorosales_@hotmail.com>
2023-08-02 17:58:25 -0300

    Guardando donacion en la base de datos y una relacion donation-usuario

--- back/src/controllers/paymentsController/paymentsController.js
+++ back/src/controllers/paymentsController/paymentsController.js
@@ -1,4 +1,5 @@
 const mercadopago = require("mercadopago");
+const Donation = require("../../models/donation");
 const { ACCESS_TOKEN } = process.env;
 const paymentsController = async (req, res) => {
   mercadopago.configure({
@@ -7,12 +8,14 @@ const paymentsController = async (req, res) => {
   const { items } = req.body;
   /* items: [
     {
+      //NECESITAMOS ID DEL USUARIO POR BODY O QUERY
       title: "Donativo a Human Conet",
       unit_price: 5000,
       currency_id: "COP",
       quantity: 1,
     },
   ], */
+  //guardar el preference_id
   try {
     const result = await mercadopago.preferences.create({
       items: items,
@@ -22,7 +25,14 @@ const paymentsController = async (req, res) => {
         pending: "localhost:3001/payments/pendig",
       },
     });
-    console.log(result.body);
+    console.log(result.body.id);
+
+    const newDonation = new Donation({
+      paymentID: result.body.id,
+      owner: "64bc515ba61ba847b6bf167e"
+    });
+    await newDonation.save()
+
     res.status(200).json({ result });
   } catch (error) {
     res.status(500).json({ error: error.message });
