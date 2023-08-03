4bc72a4e1e569635c26ad8150e5384c564fbfff1
NazaRosales <nazarenorosales_@hotmail.com>
2023-08-03 16:26:41 -0300

    Ruta failure de mercadopago

--- back/src/controllers/paymentsController/failureController.js
+++ back/src/controllers/paymentsController/failureController.js
@@ -1,25 +1,28 @@
 const transporter = require("../usersControllers/mailer");
+const Donation = require("../../models/donation");
 const { ADMIN_EMAIL } = process.env;
 const failureController = async (req, res) => {
-
-
-
-  //ELIMINAR LA DONACIÓN DE LA BASE DE DATOS
-
   //EMAIL DE INFORMAR AL USUARIO DE FALLO DE DONACION
+  const { preference_id } = req.query;
+  const donation = await Donation.findOne({
+    paymentID: preference_id,
+  }).populate("owner", "email");
+  const { email } = donation.owner;
 
-  /* await transporter.sendMail({
+  await transporter.sendMail({
     from: `"Human Conet" ${ADMIN_EMAIL}`, // sender address
     to: email, // list of receivers
-    subject: "Su donativo fue recibido", // Subject line
+    subject: "Problemas con su donativo", // Subject line
     html: `
-    <h1>Human Conet - Confirmación de donativo.</h1>
-    <p>Su donativo ha sido recibido con exito!</p>
+    <h1>Human Conet - problemas con su donativo.</h1>
+    <p>Su donativo no se ha realizado con exito.</p>
     `, // html body
   });
-  */
+
+  //ELIMINAR LA DONACIÓN DE LA BASE DE DATOS
+  await Donation.deleteOne({ paymentID: preference_id });
 
   //REDIRIGIR AL USUARIO A UNA PAGINA INFORMATIVA
- res.redirect("http://localhost:5173/rechazado")
+  res.redirect("http://localhost:5173/rechazado");
 };
 module.exports = failureController;
