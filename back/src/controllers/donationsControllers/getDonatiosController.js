d0a0bb9ad3b533d0705f17b91a0d16ef27fe13d4
NazaRosales <nazarenorosales_@hotmail.com>
2023-08-03 17:32:28 -0300

    Mercado pago + ruta get de todas las donaciones

--- back/src/controllers/donationsControllers/getDonatiosController.js
+++ back/src/controllers/donationsControllers/getDonatiosController.js
@@ -1,6 +1,7 @@
 const Donation = require("../../models/donation");
 const getDonationsControllers = async (req, res) => {
   try {
+    //Busca y devuelve todas las donaciones de la BD
     const allDonations = await Donation.find({});
     res.status(200).json(allDonations);
   } catch (error) {
