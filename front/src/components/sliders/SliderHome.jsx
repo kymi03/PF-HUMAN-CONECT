efa317a4f1ed32c91015341d3b38def5999ac65a
Ivan <125994256+pickless141@users.noreply.github.com>
2023-08-04 08:53:28 -0400

    Actualizacion de componentes

--- front/src/components/sliders/SliderHome.jsx
+++ front/src/components/sliders/SliderHome.jsx
@@ -3,62 +3,76 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-// Importa tus imágenes aquí
-import image1 from "../../assets/human-template1.png";
-import image2 from "../../assets/human-template2.png";
-import image3 from "../../assets/human-template3.png";
-import image4 from "../../assets/human-template4.png";
-import image5 from "../../assets/human-template5.png";
-import image6 from "../../assets/human-template6.png";
-import image7 from "../../assets/human-template7.png";
-
-const SlidersHome = () => {
+const SliderHome = () => {
   const settings = {
-    dots: false,
+    dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     swipe: false,
     autoplay: true,
-    autoplaySpeed: 2000,
-    centerMode: true,
+    autoplaySpeed: 1000,
     arrows: false,
   };
 
   return (
-    <div className="h-screen flex items-center justify-end">
-      <div className="w-3/4 h-full flex-grow">
-        <Slider {...settings} className="w-full h-full">
-          <div>
-            <img src={image1} alt="Image 1" className="mx-auto object-contain h-96" />
-          </div>
-          <div>
-            <img src={image2} alt="Image 2" className="mx-auto object-contain h-96" />
-          </div>
-          <div>
-            <img src={image3} alt="Image 3" className="mx-auto object-contain h-96" />
-          </div>
-          <div>
-            <img src={image4} alt="Image 4" className="mx-auto object-contain h-96" />
-          </div>
-          <div>
-            <img src={image5} alt="Image 5" className="mx-auto object-contain h-96" />
-          </div>
-          <div>
-            <img src={image6} alt="Image 6" className="mx-auto object-contain h-96" />
-          </div>
-          <div>
-            <img src={image7} alt="Image 7" className="mx-auto object-contain h-96" />
-          </div>
-          {/* Agrega más imágenes aquí */}
-        </Slider>
+    <Slider {...settings}>
+      <div>
+        <img
+          src="https://humanconet.org/wp-content/uploads/2022/09/Cover-Home-Human-Conet-01-1-scaled.webp"
+          alt="Imagen 1"
+          className="w-full h-auto rounded-2xl"
+        />
       </div>
-    </div>
+      <div>
+        <img
+          src="https://humanconet.org/wp-content/uploads/2022/05/Aliwa-Take-Action-1-01.webp"
+          alt="Imagen 2"
+          className="w-full h-auto rounded-2xl"
+        />
+      </div>
+      <div>
+        <img
+          src=""
+          alt="Imagen 3"
+          className="w-full h-auto rounded-2xl"
+        />
+      </div>
+      <div>
+        <img
+          src="https://humanconet.org/wp-content/uploads/2022/05/Aliwa-Take-Action-3-01.webp"
+          alt="Imagen 4"
+          className="w-full h-auto rounded-2xl"
+        />
+      </div>
+      <div>
+        <img
+          src="https://www.dzoom.org.es/wp-content/uploads/2019/09/paisajes-expresivos-734x489.jpg"
+          alt="Imagen 5"
+          className="w-full h-auto rounded-2xl"
+        />
+      </div>
+      <div>
+        <img
+          src="https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg"
+          alt="Imagen 6"
+          className="w-full h-auto rounded-2xl"
+        />
+      </div>
+      <div>
+        <img
+          src="https://concepto.de/wp-content/uploads/2015/03/paisaje-e1549600034372.jpg"
+          alt="Imagen 7"
+          className="w-full h-auto rounded-2xl"
+        />
+      </div>
+      {/* Agrega más imágenes aquí */}
+    </Slider>
   );
 };
 
-export default SlidersHome;
+export default SliderHome;
 
 
 
