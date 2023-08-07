475098c2c47b757749b8e717df30fcad86645394
Daniel Fuentes <danielfuentesri@gmail.com>
2023-08-07 10:47:56 -0500

    donaciones y parte del borrado logico

--- back/src/controllers/usersControllers/getAllUsersController.js
+++ back/src/controllers/usersControllers/getAllUsersController.js
@@ -2,11 +2,30 @@ const user = require('../../models/user');
 
 
 
-const getUserController = async (req, res) => {
+const getAllUserController = async (req, res) => {
+
+    const {email} = req.query;
   
     try {
+
+        if (email) {
+
+          const userFound = await user.findOne({email})
+          console.log(userFound);
+
+         if(userFound){
+
+          return res.status(200).json(userFound)
+         }else {
+
+          return res.status(404)
+         }
+
+
+        }
+
         const allUsers = await user.find();
-  
+
         // Si no se encontraron usuarios, devolver un mensaje apropiado
         if (allUsers.length === 0) {
           return res.status(404).json({ error: 'No se encontraron usuarios' });
@@ -15,16 +34,9 @@ const getUserController = async (req, res) => {
         return res.status(200).json(allUsers);
         }
         
-
-        if (allUsers.length === 0) {
-            return res.status(404).json({ error: 'No se encontraron usuarios' });
-          } if(allUsers) {
-               // Devolver la lista de usuarios encontrados
-          return res.status(200).json(allUsers);
-          }
     } catch (error) {
         res.status(500).json({error:"Error en el servidor"})
     }
  }
         
-module.exports = getUserController
+module.exports = getAllUserController
