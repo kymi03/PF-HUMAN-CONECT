046b096acc8805f15261e03919eb0e9335c50d9f
AlejoC137 <dapatinoa@unal.edu.co>
2023-08-07 10:25:57 -0500

    Fix footers

--- front/src/components/video/VideoPlayer.jsx
+++ front/src/components/video/VideoPlayer.jsx
@@ -6,7 +6,7 @@ const VideoPlayer = () => {
       <div className="w-11/12 lg:w-3/4 xl:w-1/2 rounded-lg overflow-hidden shadow-2xl shadow-blue-500/50">
         <iframe
           width="100%"
-          height="600"
+          height="400"
           src="https://www.youtube.com/embed/58eFKSi_iiA?autoplay=1"
           title="YouTube video player"
           frameborder="0"
