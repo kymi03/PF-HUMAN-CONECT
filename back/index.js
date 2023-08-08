fa8ab182882e9099c607838dec2871b6d6a37058
Nazareno Rosales <nazarenorosales_@hotmail.com>
2023-08-08 20:59:05 -0300

    inicial en foto de perfil del usuario

--- back/index.js
+++ back/index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3001;
 const app = require("./app");
 const { mongoose } = require("./db");
-const a = 0;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server raised in port: ${PORT || 3001}`);
 });
