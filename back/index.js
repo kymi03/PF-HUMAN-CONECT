fb6114a28ad757382c6b1e7c8ad590ff807663f5
Nazareno Rosales <nazarenorosales_@hotmail.com>
2023-08-08 20:41:30 -0300

    commit de prueba

--- back/index.js
+++ back/index.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 const PORT = process.env.PORT || 3001;
-const app = require('./app');
-const { mongoose } = require('./db');
-
-app.listen(PORT, '0.0.0.0', () => {
-	console.log(`Server raised in port: ${PORT || 3001}`);
+const app = require("./app");
+const { mongoose } = require("./db");
+const a = 0;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server raised in port: ${PORT || 3001}`);
 });
